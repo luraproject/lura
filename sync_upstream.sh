e4e2d4d59e19b1bfdef3aa699333832d1eda2388
Ulises Olivenza <ulises.olivenza@gmail.com>
2017-09-05 11:53:15 +0200

    Update sync upstream

--- sync_upstream.sh
+++ sync_upstream.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
-set -e
+set -x
 
-origin_url=https://github.com/devopsfaith/krakend.git
+upstream_url=https://github.com/devopsfaith/krakend.git
 branch_name="sync-upstream-"$(date +"%Y%M%d")
 
 function add_remote {
-	echo "* Adding the origin remote: $origin_url"
-	git remote add origin $origin_url
+	echo "* Adding the upstream remote: $upstream_url"
+	git remote add upstream $origin_url
 }
 
 function main {
-	origin=$(git remote | grep origin )
-
-	if [[ -z $origin ]]; then
+	upstream=$(git remote | grep upstream )
+	if [[ -z $upstream ]]; then
 		add_remote
 	fi
 
-	git checkout master
+	git checkout t-master
 
 	echo "* Fetching from upstream"
-	git pull --tags origin master
+	git pull --tags upstream master
 
 	echo "* Creating new branch $branch_name"
 	git checkout -b $branch_name
 
 	echo "* Pushing to internal github"
-	git push --tags schibsted $branch_name
+	git push --tags origin $branch_name
 }
 
 main
