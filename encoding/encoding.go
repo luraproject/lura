98699750d971e109b7fe159bbec1c4eea4a6f30b
Daniel López <kpacha666@gmail.com>
2017-11-07 17:41:44 +0100

    Remove external encoders (#52)
    * remove all the external encoding components
    
    * makefile fixed
    
    * tip version removed from the travis matrix
    
    * coverage improved

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -15,4 +15,26 @@ import "io"
 
 // A Decoder is a function that reads from the reader and decodes it
 // into an map of interfaces
-type Decoder func(r io.Reader, v *map[string]interface{}) error
+type Decoder func(io.Reader, *map[string]interface{}) error
+
+// A DecoderFactory is a function that returns CollectionDecoder or an EntityDecoder
+type DecoderFactory func(bool) Decoder
+
+var decoders = map[string]DecoderFactory{JSON: NewJSONDecoder}
+
+// Register registers the decoder factory with the given name
+func Register(name string, dec DecoderFactory) error {
+	decoders[name] = dec
+	return nil
+}
+
+// Get returns (from the register) the decoder factory by name. If there is no factory with the received name
+// it returns the JSON decoder factory
+func Get(name string) DecoderFactory {
+	for _, n := range []string{name, JSON} {
+		if dec, ok := decoders[n]; ok {
+			return dec
+		}
+	}
+	return NewJSONDecoder
+}
---
+++ encoding/encoding_test.go
@@ -0,0 +1,59 @@
+package encoding
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestRegister(t *testing.T) {
+	original := decoders
+
+	if len(decoders) != 1 {
+		t.Error("Unexpected number of registered factories:", len(decoders))
+	}
+
+	decoders = map[string]DecoderFactory{}
+	Register("some", NewJSONDecoder)
+
+	if len(decoders) != 1 {
+		t.Error("Unexpected number of registered factories:", len(decoders))
+	}
+
+	decoders = original
+}
+
+func TestGet(t *testing.T) {
+	original := decoders
+
+	if len(decoders) != 1 {
+		t.Error("Unexpected number of registered factories:", len(decoders))
+	}
+
+	checkDecoder(t, JSON)
+	checkDecoder(t, "some")
+
+	decoders = map[string]DecoderFactory{}
+	Register("some", NewJSONDecoder)
+
+	if len(decoders) != 1 {
+		t.Error("Unexpected number of registered factories:", len(decoders))
+	}
+
+	checkDecoder(t, JSON)
+	checkDecoder(t, "some")
+
+	decoders = original
+}
+
+func checkDecoder(t *testing.T, name string) {
+	d := Get(name)(false)
+
+	input := strings.NewReader(`{"foo": "bar"}`)
+	var result map[string]interface{}
+	if err := d(input, &result); err != nil {
+		t.Error("Unexpected error:", err.Error())
+	}
+	if result["foo"] != "bar" {
+		t.Error("Unexpected value:", result["foo"])
+	}
+}
