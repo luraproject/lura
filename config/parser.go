2b48e890378e83ef355590ff08f6255bdca4f67d
kpacha <kpacha666@gmail.com>
2017-11-18 19:16:19 +0100

    doc completed

--- config/parser.go
+++ config/parser.go
@@ -5,6 +5,9 @@ type Parser interface {
 	Parse(configFile string) (ServiceConfig, error)
 }
 
+// ParserFunc type is an adapter to allow the use of ordinary functions as subscribers.
+// If f is a function with the appropriate signature, ParserFunc(f) is a Parser that calls f.
 type ParserFunc func(string) (ServiceConfig, error)
 
+// Parse implements the Parser interface
 func (f ParserFunc) Parse(configFile string) (ServiceConfig, error) { return f(configFile) }
