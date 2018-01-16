bbe3a9228da548fb54644d00f0903430c34f9b8c
kpacha <kpacha666@gmail.com>
2018-01-16 23:47:41 +0100

    do not test the debug handler through an actual http server

--- router/mux/debug_test.go
+++ router/mux/debug_test.go
@@ -2,11 +2,10 @@ package mux
 
 import (
 	"bytes"
-	"context"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/devopsfaith/krakend/logging"
 )
@@ -19,46 +18,35 @@ func TestDebugHandler(t *testing.T) {
 		return
 	}
 
-	router := http.NewServeMux()
-	router.Handle("/_mux_debug", DebugHandler(logger))
-	s := &http.Server{
-		Addr:    ":8089",
-		Handler: router,
-	}
-	defer s.Shutdown(context.Background())
-	go s.ListenAndServe()
+	handler := DebugHandler(logger)
 
-	time.Sleep(5 * time.Millisecond)
-
-	req, _ := http.NewRequest("GET", "http://127.0.0.1:8089/_mux_debug?b=1", nil)
+	req, _ := http.NewRequest("GET", "http://127.0.0.1:8089/_mux_debug?b=1", ioutil.NopCloser(&bytes.Buffer{}))
 	req.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Error("sending a request:", err.Error())
-		return
-	}
-	defer resp.Body.Close()
+	w := httptest.NewRecorder()
 
-	body, ioerr := ioutil.ReadAll(resp.Body)
+	handler.ServeHTTP(w, req)
+
+	body, ioerr := ioutil.ReadAll(w.Result().Body)
 	if ioerr != nil {
 		t.Error("reading a response:", err.Error())
 		return
 	}
+	w.Result().Body.Close()
 
 	expectedBody := "{\"message\":\"pong\"}"
 
 	content := string(body)
-	if resp.Header.Get("Cache-Control") != "" {
-		t.Error("Cache-Control error:", resp.Header.Get("Cache-Control"))
+	if w.Result().Header.Get("Cache-Control") != "" {
+		t.Error("Cache-Control error:", w.Result().Header.Get("Cache-Control"))
 	}
-	if resp.Header.Get("Content-Type") != "application/json" {
-		t.Error("Content-Type error:", resp.Header.Get("Content-Type"))
+	if w.Result().Header.Get("Content-Type") != "application/json" {
+		t.Error("Content-Type error:", w.Result().Header.Get("Content-Type"))
 	}
-	if resp.Header.Get("X-Krakend") != "" {
-		t.Error("X-Krakend error:", resp.Header.Get("X-Krakend"))
+	if w.Result().Header.Get("X-Krakend") != "" {
+		t.Error("X-Krakend error:", w.Result().Header.Get("X-Krakend"))
 	}
-	if resp.StatusCode != http.StatusOK {
-		t.Error("Unexpected status code:", resp.StatusCode)
+	if w.Result().StatusCode != http.StatusOK {
+		t.Error("Unexpected status code:", w.Result().StatusCode)
 	}
 	if content != expectedBody {
 		t.Error("Unexpected body:", content, "expected:", expectedBody)
