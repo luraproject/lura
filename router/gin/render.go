84d67a69fb1d9c3bc626fc2ea041681aa46706b8
kpacha <kpacha666@gmail.com>
2018-01-23 20:50:31 +0100

    xml render also added

--- router/gin/render.go
+++ router/gin/render.go
@@ -10,17 +10,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Render defines the signature of the functions to be use for the final response
+// encoding and rendering
 type Render func(*gin.Context, *proxy.Response)
+
+// NEGOTIATE defines the value of the OutputEncoding for the negotiated render
+const NEGOTIATE = "negotiate"
 
 var (
 	renderRegister = map[string]Render{
-		"negotiate":     negotiatedRender,
+		NEGOTIATE:       negotiatedRender,
 		encoding.NOOP:   noopRender,
 		encoding.STRING: stringRender,
 		encoding.JSON:   jsonRender,
 	}
 )
 
+// RegisterRender allows clients to register their custom renders
 func RegisterRender(name string, r Render) {
 	renderRegister[name] = r
 }
@@ -53,11 +59,11 @@ func getRenderFromBackend(cfg *config.Backend) Render {
 func negotiatedRender(c *gin.Context, response *proxy.Response) {
 	switch c.NegotiateFormat(gin.MIMEJSON, gin.MIMEPlain, gin.MIMEXML) {
 	case gin.MIMEXML:
-		renderResponse(c.XML, response)
+		xmlRender(c, response)
 	case gin.MIMEPlain:
-		renderResponse(c.YAML, response)
+		yamlRender(c, response)
 	default:
-		renderResponse(c.JSON, response)
+		jsonRender(c, response)
 	}
 }
 
@@ -66,11 +72,17 @@ func stringRender(c *gin.Context, response *proxy.Response) {
 		c.String(http.StatusOK, "")
 		return
 	}
-	if d, ok := response.Data["content"].(string); ok {
-		c.String(http.StatusOK, d)
+	d, ok := response.Data["content"]
+	if !ok {
+		c.String(http.StatusOK, "")
 		return
 	}
-	c.String(http.StatusOK, "")
+	msg, ok := d.(string)
+	if !ok {
+		c.String(http.StatusOK, "")
+		return
+	}
+	c.String(http.StatusOK, msg)
 }
 
 func jsonRender(c *gin.Context, response *proxy.Response) {
@@ -81,21 +93,37 @@ func jsonRender(c *gin.Context, response *proxy.Response) {
 	c.JSON(http.StatusOK, response.Data)
 }
 
+func xmlRender(c *gin.Context, response *proxy.Response) {
+	if response == nil {
+		c.XML(http.StatusOK, nil)
+		return
+	}
+	d, ok := response.Data["content"]
+	if !ok {
+		c.XML(http.StatusOK, nil)
+		return
+	}
+	c.XML(http.StatusOK, d)
+}
+
 func noopRender(c *gin.Context, response *proxy.Response) {
 	if response == nil {
 		c.Status(http.StatusInternalServerError)
 		return
 	}
 	c.Status(response.Metadata.StatusCode)
+	if hs, ok := response.Metadata.Headers["Content-Type"]; ok && len(hs) > 0 {
+		c.Header("Content-Type", hs[0])
+	}
 	io.Copy(c.Writer, response.Io)
 }
 
-func renderResponse(render func(int, interface{}), response *proxy.Response) {
+func yamlRender(c *gin.Context, response *proxy.Response) {
 	if response == nil {
-		render(http.StatusOK, emptyResponse)
+		c.YAML(http.StatusOK, emptyResponse)
 		return
 	}
-	render(http.StatusOK, response.Data)
+	c.YAML(http.StatusOK, response.Data)
 }
 
 var emptyResponse = gin.H{}
