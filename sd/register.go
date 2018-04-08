a91201abfc639c4e41570ff9d86f274e10be75da
kpacha <kpacha666@gmail.com>
2018-04-08 02:46:07 +0200

    sd register exported

--- sd/register.go
+++ sd/register.go
@@ -1,21 +1,63 @@
 package sd
 
-import "github.com/devopsfaith/krakend/config"
+import (
+	"sync"
 
-// RegisterSubscriberFactory registers the received subscriber factory for later usage
+	"github.com/devopsfaith/krakend/config"
+)
+
+// Deprecated: RegisterSubscriberFactory. Use the GetRegister function
 func RegisterSubscriberFactory(name string, sf SubscriberFactory) error {
-	subscriberFactories[name] = sf
+	return subscriberFactories.Register(name, sf)
+}
+
+// Deprecated: GetSubscriber. Use the GetRegister function
+func GetSubscriber(cfg *config.Backend) Subscriber {
+	return subscriberFactories.Get(cfg.SD)(cfg)
+}
+
+// RegisterSetter registers the received subscriber factory for later usage
+type RegisterSetter interface {
+	Register(name string, sf SubscriberFactory) error
+}
+
+// RegisterGetter gets the subscriber factory by name or a fixed subscriber factory if
+// the name is not registered
+type RegisterGetter interface {
+	Get(name string) SubscriberFactory
+}
+
+// GetRegister returns the package register
+func GetRegister() *Register {
+	return subscriberFactories
+}
+
+// Register is a SD register
+type Register struct {
+	data  map[string]SubscriberFactory
+	mutex *sync.RWMutex
+}
+
+var subscriberFactories = &Register{
+	data:  map[string]SubscriberFactory{},
+	mutex: &sync.RWMutex{},
+}
+
+// Register implements the RegisterSetter interface
+func (r *Register) Register(name string, sf SubscriberFactory) error {
+	r.mutex.Lock()
+	r.data[name] = sf
+	r.mutex.Unlock()
 	return nil
 }
 
-// GetSubscriber gets the subscriber factory by name or a fixed subscriber factory if
-// the name is not registered
-func GetSubscriber(cfg *config.Backend) Subscriber {
-	sf, ok := subscriberFactories[cfg.SD]
+// Get implements the RegisterGetter interface
+func (r *Register) Get(name string) SubscriberFactory {
+	r.mutex.RLock()
+	sf, ok := r.data[name]
+	r.mutex.RUnlock()
 	if !ok {
-		return FixedSubscriberFactory(cfg)
+		return FixedSubscriberFactory
 	}
-	return sf(cfg)
+	return sf
 }
-
-var subscriberFactories = map[string]SubscriberFactory{}
