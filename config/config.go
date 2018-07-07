1960f85891163af2551e80e36696f5ae182cf842
kpacha <kpacha666@gmail.com>
2018-07-07 18:33:58 +0200

    parse the service name

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ var RoutingPattern = ColonRouterPatternBuilder
 // ServiceConfig defines the krakend service
 type ServiceConfig struct {
 	// name of the service
-	Name string
+	Name string `mapstructure:"name"`
 	// set of endpoint definitions
 	Endpoints []*EndpointConfig `mapstructure:"endpoints"`
 	// defafult timeout
