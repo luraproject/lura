ba3cd4a32af316b848ab084cf15f8d1785eb0064
Alexander Babenko <foxcool333@gmail.com>
2019-01-14 15:48:00 +0300

    WIP: #184 added httptreemux router adapter

---
+++ router/httptreemux/router.go
@@ -0,0 +1,48 @@
+package httptreemux
+
+import (
+	"net/http"
+
+	"github.com/devopsfaith/krakend/logging"
+	"github.com/devopsfaith/krakend/proxy"
+	"github.com/devopsfaith/krakend/router"
+	"github.com/devopsfaith/krakend/router/mux"
+	"github.com/dimfeld/httptreemux"
+)
+
+// DefaultFactory returns a net/http mux router factory with the injected proxy factory and logger
+func DefaultFactory(pf proxy.Factory, logger logging.Logger) router.Factory {
+	return mux.NewFactory(DefaultConfig(pf, logger))
+}
+
+// DefaultConfig returns the struct that collects the parts the router should be built from
+func DefaultConfig(pf proxy.Factory, logger logging.Logger) mux.Config {
+	return mux.Config{
+		Engine:         httptreemuxEngine{httptreemux.NewContextMux()},
+		Middlewares:    []mux.HandlerMiddleware{},
+		HandlerFactory: mux.CustomEndpointHandler(mux.NewRequestBuilder(httptreemuxParamsExtractor)),
+		ProxyFactory:   pf,
+		Logger:         logger,
+		DebugPattern:   "/__debug/{params}",
+		RunServer:      router.RunServer,
+	}
+}
+
+func httptreemuxParamsExtractor(r *http.Request) map[string]string {
+	return httptreemux.ContextParams(r.Context())
+
+}
+
+type httptreemuxEngine struct {
+	r *httptreemux.ContextMux
+}
+
+// Handle implements the mux.Engine interface from the krakend router package
+func (g httptreemuxEngine) Handle(pattern, method string, handler http.Handler) {
+	g.r.Handle(method, pattern, handler.(http.HandlerFunc))
+}
+
+// ServeHTTP implements the http:Handler interface from the stdlib
+func (g httptreemuxEngine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	g.r.ServeHTTP(mux.NewHTTPErrorInterceptor(w), r)
+}
