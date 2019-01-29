9525c7c558abbd4832c3f84d6a171533dbcaba0b
Aldi Priya Perdana <aldidana@gmail.com>
2019-01-30 02:45:23 +0800

    Add chi router

---
+++ router/chi/debug.go
@@ -0,0 +1,27 @@
+package chi
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/devopsfaith/krakend/logging"
+)
+
+// DebugHandler creates a dummy handler function, useful for quick integration tests
+func DebugHandler(logger logging.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Debug("Method:", r.Method)
+		logger.Debug("URL:", r.RequestURI)
+		logger.Debug("Query:", r.URL.Query())
+		logger.Debug("Headers:", r.Header)
+		body, _ := ioutil.ReadAll(r.Body)
+		r.Body.Close()
+		logger.Debug("Body:", string(body))
+
+		js, _ := json.Marshal(map[string]string{"message": "pong"})
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}
+}
