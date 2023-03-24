449b9171e1632411ec4109ca80274fe39b45a8fe
Daniel Ortiz <dortiz@krakend.io>
2023-03-24 21:47:23 +0000

    Update echo endpoint for the mux router.
    Signed-off-by: Daniel Ortiz <dortiz@krakend.io>

--- router/mux/echo.go
+++ router/mux/echo.go
@@ -8,20 +8,45 @@ import (
 	"net/http"
 )
 
+type echoResponse struct {
+	Uri         string              `json:"req_uri"`
+	UriDetails  map[string]string   `json:"req_uri_details"`
+	Method      string              `json:"req_method"`
+	Querystring map[string][]string `json:"req_querystring"`
+	Body        string              `json:"req_body"`
+	Headers     map[string][]string `json:"req_headers"`
+}
+
 // EchoHandler creates a dummy handler function, useful for quick integration tests
 func EchoHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		body, _ := io.ReadAll(r.Body)
-		r.Body.Close()
-		js, _ := json.Marshal(map[string]interface{}{
-			"Method":  r.Method,
-			"URL":     r.RequestURI,
-			"Query":   r.URL.Query(),
-			"Headers": r.Header,
-			"Body":    string(body),
+		var body string
+		if r.Body != nil {
+			tmp, _ := io.ReadAll(r.Body)
+			r.Body.Close()
+			body = string(tmp)
+		}
+		resp, err := json.Marshal(echoResponse{
+			Uri: r.RequestURI,
+			UriDetails: map[string]string{
+				"scheme":   r.URL.Scheme,
+				"user":     r.URL.User.String(),
+				"host":     r.Host,
+				"path":     r.URL.Path,
+				"query":    r.URL.Query().Encode(),
+				"fragment": r.URL.Fragment,
+			},
+			Method:      r.Method,
+			Querystring: r.URL.Query(),
+			Body:        body,
+			Headers:     r.Header,
 		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
 		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+		w.Write(resp)
 	}
 }
