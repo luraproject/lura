342c27e460968e815ab272e9f726edcb208c2706
Moritz Ploss <moritz.ploss@kivra.com>
2023-05-12 20:35:23 +0200

    add tests for error response writer
    Signed-off-by: Moritz Ploss <moritz.ploss@kivra.com>

--- router/gin/engine_test.go
+++ router/gin/engine_test.go
@@ -50,3 +50,41 @@ func TestNewEngine_contextIsPropagated(t *testing.T) {
 		t.Errorf("unexpected value: %s", string(b))
 	}
 }
+
+func TestNewEngine_paramsAreChecked(t *testing.T) {
+	engine := NewEngine(
+		config.ServiceConfig{},
+		EngineOptions{},
+	)
+
+	engine.GET("/user/:id/public", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	// should reject requests with url-encoded '?' (%3f) and '#' (%23)
+	assertResponse(t, engine, "GET", "/user/123/public", http.StatusOK, "ok")
+	assertResponse(t, engine, "GET", "/user/123%3f/public", http.StatusBadRequest, "error: encoded url params")
+	assertResponse(t, engine, "GET", "/user/123%23/public", http.StatusBadRequest, "error: encoded url params")
+}
+
+func assertResponse(t *testing.T, engine *gin.Engine, method, path string, statusCode int, body string) {
+	req, _ := http.NewRequest(method, path, http.NoBody)
+	w := httptest.NewRecorder()
+	engine.ServeHTTP(w, req)
+	resp := w.Result()
+
+	if sc := resp.StatusCode; sc != statusCode {
+		t.Errorf("unexpected status code: %d (expected %d)", sc, statusCode)
+		return
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("reading the response body: %s", err.Error())
+		return
+	}
+
+	if string(b) != body {
+		t.Errorf("unexpected response body: '%s' (expected '%s')", string(b), body)
+	}
+}
