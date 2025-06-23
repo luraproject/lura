f2850fd030c95508ef9d3644227f3d3fdbc2a826
David Hontecillas <dhontecillas@gmail.com>
2025-06-23 11:21:16 +0200

    add documentation for response wrapper
    Signed-off-by: David Hontecillas <dhontecillas@gmail.com>

--- transport/http/response_wrapper.go
+++ transport/http/response_wrapper.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 )
 
-// gin implements unwrap to get the inner response writer
+// ResponseWrapper defines the interface to access the inner
+// http.ResponseWriter (for example, gin's ResponseWriter
+// wraps a http.ResponseWriter).
 type ResponseWrapper interface {
 	Unwrap() http.ResponseWriter
 }
 
+// WrappedHTTPFlusher checks if there is a inner http.ResponseWriter
+// that implements http.Flusher, and returns it directly, if not,
+// check if the writer implements it, and returns it, and otherwise
+// it returns nil.
 func WrappedHTTPFlusher(w http.ResponseWriter) http.Flusher {
 	unwrapper, ok := w.(ResponseWrapper)
 	if ok {
