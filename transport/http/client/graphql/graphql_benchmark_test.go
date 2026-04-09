77406613540111dc4483217634ffdddf761faa99
Alexander Babenko <mail@darkfox.info>
2026-04-09 16:52:55 +0700

    update graphql benchmark test
      no_params        2 ns/op      0 B/op    0 allocs    ← static
      single_param   605 ns/op    584 B/op    9 allocs
      compound_params  700 ns/op    704 B/op   10 allocs  ← new case
      mixed           1118 ns/op    920 B/op   16 allocs
    
    Signed-off-by: Alexander Babenko <mail@darkfox.info>

--- transport/http/client/graphql/graphql_benchmark_test.go
+++ transport/http/client/graphql/graphql_benchmark_test.go
@@ -3,70 +3,58 @@
 package graphql
 
 import (
-	"strings"
 	"testing"
+
+	"github.com/luraproject/lura/v2/config"
 )
 
-// legacyInterpolate reproduces the original per-request substitution: direct map lookup.
-func legacyInterpolate(replacements [][2]string, variables map[string]interface{}, params map[string]string) {
-	for _, vs := range replacements {
-		variables[vs[0]] = params[vs[1]]
-	}
-}
-
-// fixedInterpolate is the new per-request substitution: strings.ReplaceAll loop (mirrors proxy.GeneratePath).
-func fixedInterpolate(templates [][2]string, variables map[string]interface{}, params map[string]string) {
-	for _, tmpl := range templates {
-		buff := tmpl[1]
-		for k, v := range params {
-			buff = strings.ReplaceAll(buff, "{{."+k+"}}", v)
-		}
-		variables[tmpl[0]] = buff
-	}
-}
-
-var (
-	// legacy: replacements built as [varKey, capitalizedParamKey]
-	legacyReplacements = [][2]string{
-		{"single", "Owner"},
-	}
-
-	// fixed: templates built as [varKey, "{{.Owner}}/{{.Repo}} ..."]
-	fixedTemplates = [][2]string{
-		{"single", "{{.Owner}}"},
-		{"compound", "repo:{{.Owner}}/{{.Repo}} category:Announcements"},
-	}
-
-	benchVariables = map[string]interface{}{
-		"single":   "{owner}",
-		"compound": "repo:{owner}/{repo} category:Announcements",
-		"static":   "no-params-here",
-	}
-
-	benchParams = map[string]string{
+func BenchmarkExtractor_BodyFromParams(b *testing.B) {
+	params := map[string]string{
 		"Owner": "krakend",
 		"Repo":  "lura",
 	}
-)
 
-func BenchmarkInterpolation_legacy(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vars := map[string]interface{}{}
-		for k, v := range benchVariables {
-			vars[k] = v
-		}
-		legacyInterpolate(legacyReplacements, vars, benchParams)
-	}
-}
+	for _, tc := range []struct {
+		name      string
+		variables map[string]interface{}
+	}{
+		{
+			name:      "no_params",
+			variables: map[string]interface{}{"static": "no-params-here"},
+		},
+		{
+			name:      "single_param",
+			variables: map[string]interface{}{"owner": "{owner}"},
+		},
+		{
+			name: "compound_params",
+			variables: map[string]interface{}{
+				"query": "repo:{owner}/{repo} category:Announcements",
+			},
+		},
+		{
+			name: "mixed",
+			variables: map[string]interface{}{
+				"single":   "{owner}",
+				"compound": "repo:{owner}/{repo} category:Announcements",
+				"static":   "no-params-here",
+			},
+		},
+	} {
+		cfg, _ := GetOptions(config.ExtraConfig{
+			Namespace: map[string]interface{}{
+				"type":      OperationQuery,
+				"query":     "{ search(query: $query) { nodes { id } } }",
+				"variables": tc.variables,
+			},
+		})
+		extractor := New(*cfg)
 
-func BenchmarkInterpolation_fixed(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		vars := map[string]interface{}{}
-		for k, v := range benchVariables {
-			vars[k] = v
-		}
-		fixedInterpolate(fixedTemplates, vars, benchParams)
+		b.Run(tc.name, func(b *testing.B) {
+			b.ReportAllocs()
+			for i := 0; i < b.N; i++ {
+				extractor.BodyFromParams(params)
+			}
+		})
 	}
 }
